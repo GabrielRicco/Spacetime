76fa18c5f89bb6d070a02ac2045928d04bbbd7dd
Gabriel Ricco <gabrielrdantas@outlook.com>
2023-09-15 20:53:33 -0300

    final commit

---
+++ mobile/app/index.tsx
@@ -0,0 +1,93 @@
+import { StatusBar } from 'expo-status-bar'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+import Logo from '../src/assets/logo.svg'
+
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import React, { useEffect } from 'react'
+import { api } from '../src/lib/api'
+import * as SecureStore from 'expo-secure-store'
+
+import { useRouter } from 'expo-router'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/2da94a26c91f2ec17a57',
+}
+
+export default function App() {
+  const router = useRouter()
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '2da94a26c91f2ec17a57',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    //  console.log(
+    //    makeRedirectUri({
+    //      scheme: 'nlwspacetime',
+    //    }),
+    //  )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
+
+  return (
+    <View className="flex-1 items-center px-8 py-10">
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+          onPress={() => signInWithGithub()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
+      <StatusBar style="light" translucent />
+    </View>
+  )
+}
